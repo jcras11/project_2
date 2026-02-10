eb884ff5708f5b28b6cbf292287178a001a9ab0a
jcras11 <jcras11@gmail.com>
2026-02-09 18:14:55 -0700

    Add files via upload

--- Geometry_Operations.py
+++ Geometry_Operations.py
@@ -12,48 +12,83 @@ import sys
 
 # length of the beam
 def Length(bar):
-    return
+    bar_node = bar.init_node
+    BtoV = BarNodeToVector(bar_node,bar)
+    length = VectorTwoNorm(BtoV)
+    return length
 
 # Find two norm (magnitude) of a vector
 def VectorTwoNorm(vector):
-    return
+    norm = 0
+    for i in range(0,len(vector)):
+        norm += vector[i]**2
+    return np.sqrt(norm)
 
 # Find a shared node between two bars
 def FindSharedNode(bar_1,bar_2):
-    return
+    if(bar_1.init_node==bar_2.init_node):
+        return bar_1.init_node
+    elif(bar_1.init_node==bar_2.end_node):
+        return bar_1.init_node
+    elif(bar_1.end_node==bar_2.init_node):
+        return bar_1.end_node
+    elif(bar_1.end_node==bar_2.end_node):
+        return bar_1.end_node
+    else:
+        sys.exit("The two input bars do not share a node")
 
 # Given a bar and a node on that bar, find the other node
 def FindOtherNode(node,bar):
-    return
+    if(bar.init_node == node):
+        return bar.end_node
+    elif(bar.end_node == node):
+        return bar.init_node
+    else:
+        sys.exit("The input node is not on the bar")
 
 # Find a vector from input node (of the input bar) in the direction of the bar
 def BarNodeToVector(origin_node,bar):
-    return
+    other_node = FindOtherNode(origin_node, bar)
+    origin_loc = origin_node.location
+    other_loc = other_node.location
+    vec = [other_loc[0]-origin_loc[0], other_loc[1]-origin_loc[1]]
+    return vec
 
 # Convert to bars that meet at a node into vectors pointing away from that node
 def BarsToVectors(bar_1,bar_2):
-    return
+    shared_node = FindSharedNode(bar_1, bar_2)
+    vec_1 = BarNodeToVector(shared_node, bar_1)
+    vec_2 = BarNodeToVector(shared_node, bar_2)
+    return vec_1, vec_2
 
 # Cross product of two vectors
 def TwoDCrossProduct(vec1,vec2):
-    return
+    return np.cross(vec1, vec2)
 
 # Dot product of two vectors
 def DotProduct(vec1,vec2):
-    return
+    return np.dot(vec1, vec2)
 
 # Cosine of angle from local x vector direction to other vector
 def CosineVectors(local_x_vec,other_vec):
-    return
+    dot = DotProduct(local_x_vec, other_vec)
+    cosine = dot/(VectorTwoNorm(local_x_vec)*VectorTwoNorm(other_vec))
+    return cosine
 
 # Sine of angle from local x vector direction to other vector
 def SineVectors(local_x_vec,other_vec):
-    return
+    cross = TwoDCrossProduct(local_x_vec, other_vec)
+    sine = cross/(VectorTwoNorm(local_x_vec)*VectorTwoNorm(other_vec))
+    return sine
 
 # Cosine of angle from local x bar to the other bar
 def CosineBars(local_x_bar,other_bar):
-    return
+    vec_1, vec_2 = BarsToVectors(local_x_bar, other_bar)
+    cosine = CosineVectors(vec_1, vec_2)
+    return cosine
 
 # Sine of angle from local x bar to the other bar
 def SineBars(local_x_bar,other_bar):
-    return
+    vec_1, vec_2 = BarsToVectors(local_x_bar, other_bar)
+    sine = SineVectors(vec_1, vec_2)
+    return sine
--- Structure_Operations.py
+++ Structure_Operations.py
@@ -5,7 +5,6 @@ Created on Wed Jul 14 14:34:19 2021
 
 @author: kendrick shepherd
 """
-
 import sys
 
 # determine if the bar is statically determinate (and belongs to a truss)
@@ -14,7 +13,7 @@ def StaticallyDeterminate(nodes,bars):
     n_nodes = len(nodes)
     n_bars = len(bars)
     
-    # Determine number of (valid) reactions supported by nodes of the truss
+# Determine number of (valid) reactions supported by nodes of the truss
     n_reactions = 0
     for node in nodes:
         if(any(node.ConstraintType())):
@@ -25,9 +24,9 @@ def StaticallyDeterminate(nodes,bars):
             else:
                 n_reactions += len(node.ConstraintType())
     
-    # Compute if b + r = 2j (Equation 3-1 of the textbook)
+# Compute if b + r = 2j (Equation 3-1 of the textbook)
     if(n_bars + n_reactions < 2*n_nodes):
-        sys.exit("The truss is unstable")
+        sys.exit("The truss is unstable; did you input all of the reaction constraints correctly?")
     elif(n_bars + n_reactions > 2*n_nodes):
         sys.exit("The truss is statically indeterminate, and cannot be resolved using method of joints")
     else:
@@ -52,10 +51,50 @@ def ComputeReactions(nodes):
         sys.exit("A more clever way must be found to compute the reaction forces")
     
     # Continue from here
-    # Sum of moments about the pin
-
-    # sum of forces in y direction
-
-    # sum of forces in x direction
     
+# Sum of moments about the pin
+    [pin_x, pin_y] = pin_node.location
+    [roller_x, roller_y] = roller_node.location
+    roller_reaction = 0
+    for node in nodes:
+        [node_x,node_y] = node.location
+        roller_reaction += node.yforce_external * (node_x - pin_x)
+        roller_reaction += node.xforce_external * (pin_y - node_y)
     
+#This section basically says that if there's no x displacement, then there has to be
+#a reaction force in the x direction
+    if(roller_node.constraint=="roller_no_xdisp"):
+        roller_reaction = -roller_reaction/(pin_y - roller_y)
+        roller_node.AddReactionXForce(roller_reaction)
+        
+#This section is the same except for with the y displacement
+    elif(roller_node.constraint=="roller_no_ydisp"):
+        roller_reaction = -roller_reaction/(roller_x - pin_x)
+        roller_node.AddReactionYForce(roller_reaction)
+       
+# sum of forces in y direction
+    sum_of_y = 0
+    for node in nodes:
+        sum_of_y += node.yforce_external
+    
+# Compute pin y-reaction. If there's not ydisplacement at the roller, that means there's
+# a reaction force there, so we subtract it from the sum of the forces in the y direction
+    if(roller_node.constraint=="roller_no_ydisp"):
+        pin_y_reaction = -sum_of_y - roller_reaction
+#If there is ydisplacement, then the only reaction force is that of the pin , shown below       
+    else:
+        pin_y_reaction = -sum_of_y 
+#assign it to the respective attribute as the node
+    pin_node.AddReactionYForce(pin_y_reaction)
+    
+# sum of forces in x direction (same logic as the y)
+    sum_of_x = 0
+    for node in nodes:
+        sum_of_x += node.xforce_external
+    
+# Compute pin x-reaction  
+    if(roller_node.constraint=="roller_no_xdisp"):
+        pin_x_reaction = -sum_of_x - roller_reaction
+    else:
+        pin_x_reaction = -sum_of_x 
+    pin_node.AddReactionXForce(pin_x_reaction)
