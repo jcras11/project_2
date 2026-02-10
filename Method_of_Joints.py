08c7dbf6472566883469df78a829a56a6dd27af7
jcras11 <jcras11@gmail.com>
2026-02-09 18:17:05 -0700

    Add files via upload

--- Method_of_Joints.py
+++ Method_of_Joints.py
@@ -3,7 +3,7 @@
 """
 Created on Wed Jul 14 12:37:32 2021
 
-@author: kendrick shepherd
+@author: kendrick shepherd (and also Jacob Rasmussen woot woot, bust mostly Dr. Shepherd)
 """
 
 import sys
@@ -12,7 +12,11 @@ import Geometry_Operations as geom
 
 # Determine the unknown bars next to this node
 def UnknownBars(node):
-    return
+    list_of_unknown_bars = []
+    for bar in node.bars:
+        if bar.is_computed == False:
+            list_of_unknown_bars.append(bar)
+    return list_of_unknown_bars
 
 # Determine if a node if "viable" or not
 def NodeIsViable(node):
@@ -27,7 +31,30 @@ def SumOfForcesInLocalX(node, local_x_bar):
 # forces in the y direction
 def SumOfForcesInLocalY(node, unknown_bars):
     return
-    
+#Check for unknown forces
+def UnknownMemberFinder(bars):
+    for bar in bars:
+        if bar.is_computed == False:
+            return True
+    return False
+            
+#def DoIHaveAnUnknownMember(nodes):
+#    for node in nodes:
+#        unknown_bars = UnknownBars(node)
+#        if len(unknown_bars) >0:
+#            return True
+#    return False
+
 # Perform the method of joints on the structure
 def IterateUsingMethodOfJoints(nodes,bars):
-    return
+    
+    counter = 0
+    
+    while UnknownMemberFinder(bars) == True:
+        
+        
+        BIGNUMBER = len(bars) +1
+        counter += 1
+        if counter > BIGNUMBER:
+            sys.exit("Too many iterations")
+    return bars
